8b80e80bd54a7b7658cc4bfb473825554b385207
meyir moshe <mma088651259@gmail.com>
2026-02-05 11:20:47 +0200

    all is ready

--- app/dal.py
+++ app/dal.py
@@ -1,6 +1,3 @@
-
-
-
 def convert_objectId_to_string(doc):
     if doc and "_id" in doc:
         doc["_id"] = str(doc["_id"])
@@ -12,21 +9,50 @@ def convert_objectId_to_string_of_all_docs(docs):
 
 
 
+
 def get_engineering_high_salary_employees(conn):
-    pass
+    data = []
+    for item in conn.find({"job_role.department":"Engineering","salary":{"$gt":65000}},{"_id":0,"employee_id":1,"name":1,"salary":1}):
+        data.append(convert_objectId_to_string(item))
+    return data
+
 
 def get_employees_by_age_and_role(conn):
-    pass
+    data = []
+    for item in conn.find({"$or":[{"job_role.title":"Engineer"},{"job_role.title":"Specialist"}],"age":{"$gt":29,"$lt":46}}):
+        data.append(convert_objectId_to_string(item))
+    return data
+
 
 def get_top_seniority_employees_excluding_hr(conn):
-    pass
+    data = []
+    for item in conn.find({"job_role.department":{"$ne":"HR"}}).sort("years_at_company", -1).limit(7):
+        data.append(convert_objectId_to_string(item))
+    return data
+
+
+
 
 def get_employees_by_age_or_seniority(conn):
-    pass
+    data = []
+    for item in conn.find({"$or":[{"age":{"$gt":50}},{"years_at_company":{"$lt":3}}]},{"_id":0,"employee_id":1,"name":1,"age":1,"years_at_company":1}):
+        data.append(convert_objectId_to_string(item))
+    return data
+
+
 
 def get_managers_excluding_departments(conn):
-    pass
+    data = []
+    for item in conn.find({"job_role.title":"Manager","job_role.department":{"$nin":["Marketing","Sales"]}}):
+        data.append(convert_objectId_to_string(item))
+    return data
+
+
 
 
 def get_employees_by_lastname_and_age(conn):
-    pass
+    data = []
+    for item in conn.find({"$or": [{"name":{"$regex":"Nelson$"}}, {"name":{"$regex":"Wright$"}}],"age":{"$lt":35}},{"_id": 0,"name": 1, "age": 1, "job_role.department": 1}):
+        data.append(convert_objectId_to_string(item))
+    return data
+
--- app/routes.py
+++ app/routes.py
@@ -6,33 +6,33 @@ route = APIRouter(prefix="/employees")
 
 
 @route.get("/engineering/high-salary")
-def get_all():
+def engineering_high_salary_employees():
     conn = get_con_db()
     return get_engineering_high_salary_employees(conn)
 
 @route.get("/by-age-and-role")
-def get_all():
+def employees_by_age_and_role():
     conn = get_con_db()
     return get_employees_by_age_and_role(conn)
 
 
 @route.get("/top-seniority")
-def get_all():
+def top_seniority_employees_excluding_hr():
     conn = get_con_db()
     return get_top_seniority_employees_excluding_hr(conn)
 
 @route.get("/age-or-seniority")
-def get_all():
+def employees_by_age_or_seniority():
     conn = get_con_db()
     return get_employees_by_age_or_seniority(conn)
 
 @route.get("/managers/excluding-departments")
-def get_all():
+def managers_excluding_departments():
     conn = get_con_db()
     return get_managers_excluding_departments(conn)
 
 @route.get("/by-lastname-and-age")
-def get_all():
+def employees_by_lastname_and_age():
     conn = get_con_db()
     return get_employees_by_lastname_and_age(conn)
 
