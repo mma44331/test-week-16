ffd5260fb5b37180ca77836003d3248e689fbad6
meyir moshe <mma088651259@gmail.com>
2026-02-05 13:10:03 +0200

    d

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
+import json
+from os import getenv
+
 from fastapi import FastAPI
 import uvicorn
+from pymongo import MongoClient
+
 from routes import route
 from json_to_mongo import create_db
 
@@ -9,7 +14,28 @@ app.include_router(route)
 
 @app.on_event("startup")
 def create():
-    create_db()
+    mongo_uri = getenv("MONGO_URL", "mongodb://mongodb-gerstnir-dev.apps.rm2.thpm.p1.openshiftapps.com:27017/")
+    mongo_db = getenv("MONGO_DB", "mymongodb")
+    mongo_collection = getenv("MONGO_COLLECTION", "employee_data_advanced")
+    file_path = './employee_data_advanced.json'
+
+    # Making Connection
+    myclient = MongoClient(mongo_uri)
+
+    # database
+    db = myclient[mongo_db]
+
+    # Created or Switched to collection
+    # names: GeeksForGeeks
+    Collection = db[mongo_collection]
+
+    # Loading or Opening the json file
+    with open(file_path) as file:
+        file_data = json.load(file)
+
+    # Inserting the loaded data in the Collection
+    ins_result = Collection.insert_many(file_data)
+    print(f"Data inserted to MongoDB. Documents inserted: {len(ins_result.inserted_ids)}")
 
 
 
